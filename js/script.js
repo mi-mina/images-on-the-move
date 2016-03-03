ffdbfb30d93e7022cfaedd3e7d3daa5dd1ba0639
Esperanza Moreno Cruz <hola@mi-mina.com>
2016-03-03 21:39:14 +0100

    wip

---
+++ js/script.js
@@ -0,0 +1,86 @@
+var theBody = document.getElementsByTagName("body")[0];
+var theImg = document.createElement("img");
+var patterns = ["circles", "triangles", "flowers"];
+var pattern_ix = 0;
+var pattern_img, pattern_bg, pos_ix, pos_iy;
+var images = document.getElementsByTagName("img");
+var n_img = 4;
+var the_timer; //Has to be a global var
+
+function load_images(){
+  //We stop the interval in case it is running
+  clearInterval(the_timer);
+  // We need to clear the existing images, in case that there are some.
+  if(images[0]){ //I check if some images are already in images[]
+    for(var i = 0; i < n_img ; i++){ //Important! Do not use i < images.length,
+    //because we are removing elements and the value changes in each iteration
+    images[0].parentNode.removeChild(images[0]); //Do not use images[i],
+    //because when we remove the first element, the last index i won't exist.
+    }
+  }
+  //Changing the background and the images
+  pattern_img = String(patterns[pattern_ix] + ".png");
+  pattern_bg = String (patterns[pattern_ix] + "-bg.png");
+  theBody.style.backgroundImage= String("url(img/" + pattern_bg + ")");
+  theImg.src = String("img/" + pattern_img);
+
+  //Start position
+  pos_ix = window.innerWidth/2 - theImg.width/2;
+  pos_iy = window.innerHeight/2 - theImg.height/2;
+
+  //Background style
+  theBody.style.backgroundPosition= String(pos_ix + " "+ pos_iy);
+  theBody.style.backgroundRepeat = "no-repeat";
+
+  //Image style
+  theImg.style.position="absolute";
+  theImg.style.opacity=1;
+  theImg.style.left=pos_ix;
+  theImg.style.top=pos_iy;
+
+  //Create images
+  for (var i = 0; i < n_img; i++) {
+    theBody.appendChild(theImg.cloneNode(true));
+    }
+  }
+
+function move () {
+  var speeds = [];
+    for(var i = 0; i < n_img; i++){
+      speeds.push((Math.random()*2)+1);
+    }
+  var x_pos = [];
+    for(var i = 0; i < n_img; i++){
+      x_pos.push(pos_ix);
+    }
+  var y_pos = [];
+    for(var i = 0; i < n_img; i++){
+      y_pos.push(pos_iy);
+    }
+
+  function display(){
+    for(var i = 0; i < images.length; i++){
+      if(i % 2 == 0 && i % 4 == 2){
+        x_pos[i] = x_pos[i] + speeds[i];
+      }else if(i % 4 == 0){
+        x_pos[i] = x_pos[i] - speeds[i];
+      }else if((i-1) % 2 == 0 && (i-1) % 4 == 2){
+        y_pos[i] = y_pos[i] + speeds[i];
+      }else{
+        y_pos[i] = y_pos[i] - speeds[i];
+      }
+      images[i].style.left=x_pos[i];
+      images[i].style.top=y_pos[i];
+    }
+  }
+  the_timer = setInterval(display, 50);
+}
+//EVENTS
+var select = document.getElementById("pattern");
+function change_pattern(){
+  var pattern_choosen = document.getElementById("pattern").value;
+  pattern_ix = patterns.indexOf(pattern_choosen);
+  console.log("pattern_id: " + pattern_ix);
+  load_images();
+}
+select.addEventListener("change", change_pattern, false);
