e609836d8ba9e17bdcf6940774dec2c15b72d9e7
Esperanza Moreno Cruz <hola@mi-mina.com>
2016-03-06 19:26:13 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 var theBody = document.getElementsByTagName("body")[0];
 var theImg = document.createElement("img");
-var patterns = ["circles", "triangles", "flowers"];
+var patterns = ["circles", "triangles", "hexagones", "flowers"];
 var pattern_ix = 0;
 var pattern_img, pattern_bg, pos_ix, pos_iy;
 var images = document.getElementsByTagName("img");
 var n_img = 4;
 var the_timer; //Has to be a global var
 
-function load_images(){
-  //We stop the interval in case it is running
-  clearInterval(the_timer);
-  // We need to clear the existing images, in case that there are some.
+function create_options(){
+  var select = document.getElementById("select");
+
+  for(var i in patterns){
+    var option = document.createElement("option");
+    option.appendChild(document.createTextNode(patterns[i]));
+    option.setAttribute("value", patterns[i]);
+    if(i==0){
+      option.setAttribute("selected", "");
+    }
+    select.appendChild(option);
+  }
+}
+
+function clear_images () {
+  // console.log("borrando imagenes");
   if(images[0]){ //I check if some images are already in images[]
     for(var i = 0; i < n_img ; i++){ //Important! Do not use i < images.length,
     //because we are removing elements and the value changes in each iteration
@@ -18,25 +30,29 @@ function load_images(){
     //because when we remove the first element, the last index i won't exist.
     }
   }
+}
+
+function load_images(){
+  // console.log("cargando imagenes");
   //Changing the background and the images
-  pattern_img = String(patterns[pattern_ix] + ".png");
-  pattern_bg = String (patterns[pattern_ix] + "-bg.png");
-  theBody.style.backgroundImage= String("url(img/" + pattern_bg + ")");
-  theImg.src = String("img/" + pattern_img);
+  pattern_img = patterns[pattern_ix] + ".png";
+  pattern_bg = patterns[pattern_ix] + "-bg.png";
+  theBody.style.backgroundImage= "url(img/" + pattern_bg + ")";
+  theImg.src = "img/" + pattern_img;
 
   //Start position
   pos_ix = window.innerWidth/2 - theImg.width/2;
   pos_iy = window.innerHeight/2 - theImg.height/2;
 
   //Background style
-  theBody.style.backgroundPosition= String(pos_ix + " "+ pos_iy);
+  theBody.style.backgroundPosition= pos_ix + "px "+ pos_iy + "px";
   theBody.style.backgroundRepeat = "no-repeat";
 
   //Image style
   theImg.style.position="absolute";
   theImg.style.opacity=1;
-  theImg.style.left=pos_ix;
-  theImg.style.top=pos_iy;
+  theImg.style.left=pos_ix + "px";
+  theImg.style.top=pos_iy + "px";
 
   //Create images
   for (var i = 0; i < n_img; i++) {
@@ -44,7 +60,16 @@ function load_images(){
     }
   }
 
+  function on_load(){
+    // console.log("on load");
+    create_options();
+    clearInterval(the_timer);
+    clear_images();
+    load_images();
+  }
+
 function move () {
+  // console.log("move");
   var speeds = [];
     for(var i = 0; i < n_img; i++){
       speeds.push((Math.random()*2)+1);
@@ -59,6 +84,7 @@ function move () {
     }
 
   function display(){
+    // console.log("display");
     for(var i = 0; i < images.length; i++){
       if(i % 2 == 0 && i % 4 == 2){
         x_pos[i] = x_pos[i] + speeds[i];
@@ -69,18 +95,22 @@ function move () {
       }else{
         y_pos[i] = y_pos[i] - speeds[i];
       }
-      images[i].style.left=x_pos[i];
-      images[i].style.top=y_pos[i];
+      images[i].style.left=x_pos[i]+"px";
+      images[i].style.top=y_pos[i]+"px";
     }
   }
   the_timer = setInterval(display, 50);
 }
 //EVENTS
-var select = document.getElementById("pattern");
+var select = document.getElementById("select");
+
 function change_pattern(){
-  var pattern_choosen = document.getElementById("pattern").value;
+  var pattern_choosen = document.getElementById("select").value;
   pattern_ix = patterns.indexOf(pattern_choosen);
-  console.log("pattern_id: " + pattern_ix);
+  clearInterval(the_timer);
+  clear_images();
   load_images();
+  // console.log("pattern changed");
 }
+
 select.addEventListener("change", change_pattern, false);
